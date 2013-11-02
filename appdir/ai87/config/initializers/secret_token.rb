588658fd19ccacfb15a1ec03a78cc26b4031dc6f
Meghbartma Gautam <skyspeak@gmail.com>
2013-11-02 11:50:11 -0700

    Init

---
+++ appdir/ai87/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ai87::Application.config.secret_key_base = '39af852ff1cc8863df9fa40845994f60e9168669725de0fbe1811d5c04aa9d4702fd077a80e8c6366a867a464c1b11e0623b14a5a84ea3a6a8b50e48e0eda413'
---
+++ appdir/ai87/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ai87::Application.config.session_store :cookie_store, key: '_ai87_session'
