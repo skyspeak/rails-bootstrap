112c0b9649e1147323175de54be4e46ee6019026
Meghbartma Gautam <skyspeak@gmail.com>
2013-11-05 17:39:17 -0800

    add gems

---
+++ appdir/rails-bootstrap/rails-bootstrap/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBootstrap::Application.config.secret_key_base = '34f63f36e94a566e04cd1973a2464c82be6ddf7b15f486e0bcb8792868549710967ad5a07e8c6ed744fee186e8b9d1e7d4ea85e0bf0244124c53ed69e6bb7db1'
